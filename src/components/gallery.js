f2433a470b06df54d31a2b40e9837c97745ceea5
Daknya Bam <daknyabam18@gmail.com>
2023-08-06 00:58:08 +0530

    gallery section modified

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,19 +1,61 @@
 import React from "react";
 import "../style/Gallery.scss"
-
+// import img1 from "../media/testimg/1.jpeg"
+// import img2 from "../media/testimg/2.jpg"
+// import img3 from "../media/testimg/3.jpg"
+// import img4 from "../media/testimg/4.jpg"
+// import img5 from "../media/testimg/5.jpg"
 const Gallery = () => {
 
     return (
         <>
             <div className="gallery">
 
+                {/* <div className="topImgs">
+                    <img src={img1} alt="itsandimagefuck"></img>
+                    <img src={img2} alt="itsandimagefuck"></img>
+                    <img src={img5} alt="itsandimagefuck"></img>
+                    <img src={img3} alt="itsandimagefuck"></img>
+                    <img src={img4} alt="itsandimagefuck"></img>
+                    <img src={img5} alt="itsandimagefuck"></img>
+
+                </div> */}
+                {/* <div className="topImgs">
+                    <img src={img1} alt="itsandimagefuck"></img>
+                    <img src={img2} alt="itsandimagefuck"></img>
+                    <img src={img5} alt="itsandimagefuck"></img>
+                    <img src={img3} alt="itsandimagefuck"></img>
+                    <img src={img4} alt="itsandimagefuck"></img>
+                    <img src={img5} alt="itsandimagefuck"></img>
+
+                </div> */}
                 <h1>
                     Join us and Laugh ,smile, code  with US
                     <hr />
                 </h1>
-                <div className="photos">
+                {/* <div className="topImgs">
+                    <img src={img1} alt="itsandimagefuck"></img>
+                    <img src={img2} alt="itsandimagefuck"></img>
+                    <img src={img5} alt="itsandimagefuck"></img>
+                    <img src={img3} alt="itsandimagefuck"></img>
+                    <img src={img4} alt="itsandimagefuck"></img>
+                    <img src={img5} alt="itsandimagefuck"></img>
 
                 </div>
+                <div className="botImgs">
+
+
+
+                    <img src={img3} alt="itsandimagefuck"></img>
+                    <img src={img4} alt="itsandimagefuck"></img>
+                    <img src={img5} alt="itsandimagefuck"></img>
+                    <img src={img2} alt="itsandimagefuck"></img>
+                    <img src={img1} alt="itsandimagefuck"></img>
+                    <img src={img5} alt="itsandimagefuck"></img>
+
+
+
+                </div> */}
 
 
             </div>
