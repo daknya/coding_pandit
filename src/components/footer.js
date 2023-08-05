da5c18632e39cffe6b2399498c31edc4108b3998
Daknya Bam <daknyabam18@gmail.com>
2023-08-06 00:58:40 +0530

    footer added

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,6 +6,24 @@ const Footer = () => {
         <>
             <div className="footer">
                 <div className="footercnt">
+                    <div className="extlink">
+                        <ul>
+                            <li><a href="#"> nitap home  </a></li>
+                            <li><a href="#"> gfg  </a></li>
+                            <li><a href="#"> Leet Code  </a></li>
+                            <li><a href="#"> Indiabix  </a></li>
+                        </ul></div>
+                    <div className="contacts">
+
+                        <div> 9875639472 </div>
+                        <div> codingpandit@nitap.ac.in </div>
+
+                    </div>
+                    <div className="other">
+                        <div> lorem3</div>
+                        <div> lorem3</div>
+                        <div> lorem3</div>
+                    </div>
                 </div>
                 <div className="credit">developed by team for one</div>
 
